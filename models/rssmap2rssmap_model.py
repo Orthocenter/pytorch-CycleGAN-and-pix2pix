8f742fdd11a1ba508cab4626c7ad482ca3417c28
Yuxin Chen <chenyuxin.mail@gmail.com>
2019-05-20 03:37:52 -0500

    force task only

--- models/rssmap2rssmap_model.py
+++ models/rssmap2rssmap_model.py
@@ -48,19 +48,16 @@ class RssMap2RssMapModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake', 'T_A']
+        self.loss_names = ['T_A']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['real_A', 'fake_B', 'real_B']
+        self.visual_names = ['real_A']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
-        self.text_names = ['tx_loc_pwr', 'task_A']
+        self.text_names = []
 
         if self.isTrain:
-            self.model_names = ['G', 'D', 'T']
+            self.model_names = ['T']
         else:  # during test time, only load G
-            self.model_names = ['G', 'T']
-        # define networks (both generator and discriminator)
-        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
-                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+            self.model_names = ['T']
 
         blocked_size = opt.blocked_size
         self.mask = torch.ones((1, 1, 64, 64)).float().cuda()
@@ -68,22 +65,11 @@ class RssMap2RssMapModel(BaseModel):
 
         self.netT = networks.define_T(init_type=opt.init_type, init_gain=opt.init_gain, gpu_ids=opt.gpu_ids, mask=self.mask)
 
-        if self.isTrain:  # define a discriminator; only single channel input here 
-            assert(opt.input_nc == opt.output_nc)
-            self.netD = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
-                                          opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain,self.gpu_ids, mask=self.mask)
-
         if self.isTrain:
             # define loss functions
-            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
-            self.criterionL1 = torch.nn.L1Loss()
             self.criterionT = torch.nn.SmoothL1Loss()
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
-            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizer_T = torch.optim.Adam(self.netT.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999)) 
-            self.optimizers.append(self.optimizer_G)
-            self.optimizers.append(self.optimizer_D)
             self.optimizers.append(self.optimizer_T)
 
     def set_input(self, input):
@@ -102,36 +88,8 @@ class RssMap2RssMapModel(BaseModel):
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        self.fake_B = self.netG(self.real_A)  # G(A)
         if not self.opt.isTrain: # this is for testing only; during training, we will get
             self.task_A = self.netT(self.real_A) # T(A)
-            self.task_B = self.netT(self.fake_B) # T(G(A))
-
-    def backward_D(self):
-        """Calculate GAN loss for the discriminator"""
-        # Fake; stop backprop to the generator by detaching fake_B
-        fake_B = self.fake_B
-        pred_fake = self.netD(fake_B.detach())
-        self.loss_D_fake = self.criterionGAN(pred_fake, False)
-        # Real
-        real_B = self.real_B
-        pred_real = self.netD(real_B)
-        self.loss_D_real = self.criterionGAN(pred_real, True)
-        # combine loss and calculate gradients
-        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
-        self.loss_D.backward()
-
-    def backward_G(self):
-        """Calculate GAN and L1 loss for the generator"""
-        # First, G(A) should fake the discriminator
-        fake_B = self.fake_B
-        pred_fake = self.netD(fake_B)
-        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
-        # Second, G(A) = B
-        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1
-        self.loss_G.backward()
 
     def backward_T(self):
         """Calculate TaskNetwork loss"""
@@ -141,29 +99,9 @@ class RssMap2RssMapModel(BaseModel):
         self.loss_T_A.backward(retain_graph=True)
         self.optimizer_T.step()             # udpate T's weights
 
-        # self.optimizer_T.zero_grad()        # set T's gradients to zero
-        # self.task_B = task_B = self.netT(self.fake_B) # T(G(A))
-        # self.loss_T_B = self.criterionT(task_B, self.tx_loc_pwr)
-        # self.loss_T_B *= self.opt.lambda_T
-        # self.loss_T_B.backward(retain_graph=True)
-        # self.optimizer_T.step()
-
     def optimize_parameters(self):
         self.forward()                   # compute fake images: G(A)
-        # update D
-        self.set_requires_grad(self.netD, True)  # enable backprop for D
-        self.optimizer_D.zero_grad()     # set D's gradients to zero
-        self.backward_D()                # calculate gradients for D
-        self.optimizer_D.step()          # update D's weights
 
         # update T
         self.set_requires_grad(self.netT, True)  # D requires no gradients when optimizing T
-        self.optimizer_G.zero_grad()        # set G's gradients to zero
         self.backward_T()                   # calculate graidents for T
-
-        # update G
-        self.set_requires_grad(self.netD, False)  # D requires no gradients when optimizing G
-        self.set_requires_grad(self.netT, False)  # T requires no gradients when optimizing G
-        self.backward_G()                   # calculate graidents for G
-        self.optimizer_G.step()             # udpate G's weights
-
