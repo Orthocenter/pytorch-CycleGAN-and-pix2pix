f5899ee0a28f39c2f0bb952cbdcb15c57a15c706
Yuxin Chen <chenyuxin.mail@gmail.com>
2019-05-24 03:07:11 -0500

    add expr scripts

---
+++ expr/expr12.1.sh
@@ -0,0 +1,23 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=1 \
+    --dataroot=/mnt/data/yxchen/rss-datasets/expr8/train \
+    --name=rss_expr12.1 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=64 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --lambda_L1=0 \
+    --lambda_T=100 \
+    --blocked_size=22 \
+    --max_B_size=50 \
+    --lr=0.00004 \
+    --display_env=expr12.1 \
+    --niter=250 \
+    --niter_decay=150 \
+    --verbose
---
+++ expr/expr14.2.sh
@@ -0,0 +1,22 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=3 \
+    --dataroot=/mnt/data/yxchen/rss-datasets/expr14.2/train \
+    --name=rss_expr14.2 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=256 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --lambda_L1=0 \
+    --lambda_T=100 \
+    --blocked_size=0 \
+    --lr=0.00004 \
+    --display_env=expr14.2 \
+    --niter=50 \
+    --niter_decay=150 \
+    --verbose
