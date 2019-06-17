20a720ac6393412ab89def662ca6dafd037cf0a1
aszkid <polgomezriquelme@gmail.com>
2019-06-17 10:41:56 -0500

    First training script for v3

---
+++ expr/v3_1.sh
@@ -0,0 +1,20 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=3 \
+    --dataroot=/mnt/data/yanzi/input_synthetic_train_gamma_5.0 \
+    --name=rss_v3_1 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=64 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --display_env=v3_1 \
+    --niter=100 \
+    --niter_decay=100 \
+    --max_B_size=0 \
+    --lr=0.0001 \
+    --verbose
