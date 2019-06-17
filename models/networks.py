5461814fc9f7368e884e4910f8f99d36e683862f
aszkid <polgomezriquelme@gmail.com>
2019-06-17 14:53:48 -0500

    Fixed?

--- models/networks.py
+++ models/networks.py
@@ -572,7 +572,7 @@ latent_val = None
 def capture_latent(name):
     def hook(model, input, output):
         global latent_val
-        latent_val = output # to detach or not to detach, that is the question
+        latent_val = output.data # to detach or not to detach, that is the question
         #print("!! captured latent coord: {}".format(output[:,0:2].squeeze()))
     return hook
 
