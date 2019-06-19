fad334a92179c60ad889c9b8e3e95744d8fc5f73
aszkid <polgomezriquelme@gmail.com>
2019-06-19 12:22:29 -0500

    Trying out smaller learning rate + all losses

---
+++ expr/v3_5.sh
@@ -0,0 +1,19 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=1 \
+    --dataroot=/home/gomezp/input_synthetic_train_gamma_5.0 \
+    --name=rss_v3_5 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=64 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --display_env=v3_5 \
+    --niter=50 \
+    --niter_decay=150 \
+    --lr=0.00005 \
+    --verbose
