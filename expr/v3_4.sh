8e6e9a6379ba3ff303c3c9cc71ec1f70319ef343
aszkid <polgomezriquelme@gmail.com>
2019-06-19 10:30:02 -0500

    Preparing for training on full loss

---
+++ expr/v3_4.sh
@@ -0,0 +1,19 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=1 \
+    --dataroot=/home/gomezp/input_synthetic_train_gamma_5.0 \
+    --name=rss_v3_4 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=64 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --display_env=v3_4 \
+    --niter=100 \
+    --niter_decay=100 \
+    --lr=0.00004 \
+    --verbose
