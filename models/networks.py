0d380301971f0df207aca2741a3353e7dbe90074
aszkid <polgomezriquelme@gmail.com>
2019-06-20 11:41:28 -0500

    Fix

--- models/networks.py
+++ models/networks.py
@@ -643,7 +643,9 @@ class UnetSkipConnectionBlock(nn.Module):
     def forward(self, x):
         if self.outermost:
             out = self.model(x)
-            return (out, latent_val.clone().retain_grad().squeeze())
+            latent = latent_val.clone()
+            latent.retain_grad()
+            return (out, latent.squeeze())
         else:   # add skip connections
             return torch.cat([x, self.model(x)], 1)
 
