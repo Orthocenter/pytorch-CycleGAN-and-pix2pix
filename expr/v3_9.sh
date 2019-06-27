df7beb40846556f041935b09dea64fa273e91614
aszkid <polgomezriquelme@gmail.com>
2019-06-27 12:48:01 -0500

    v3_9: synthetic gamma=2.0 (A) and real collected (B)

---
+++ expr/v3_9.sh
@@ -0,0 +1,19 @@
+python train.py \
+    --seed=666 \
+    --gpu_ids=3 \
+    --dataroot=/home/gomezp/rss-data/v3_9 \
+    --name=rss_v3_9 \
+    --model=rssmap2rssmap \
+    --input_nc=1 \
+    --output_nc=1 \
+    --norm=batch \
+    --dataset_mode=rss \
+    --num_threads=12 \
+    --batch_size=64 \
+    --gan_mode=square \
+    --netG=unet_64 \
+    --display_env=v3_9 \
+    --niter=50 \
+    --niter_decay=150 \
+    --lr=0.0001 \
+    --verbose
